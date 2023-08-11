504a9f0dbaa6a349a0634882c82b67c4fc5813ca
Ashkan Rezaei <ashkan.rezaei.tsh@gmail.com>
2023-08-11 18:21:25 +0330

    fixed login and added logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,19 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = User::query()->where('email', $request->email)->firstOrFail();
             $token = $request->user()->createToken($user->name);
 
-            return response(['token' => $token->plainTextToken]);
+            return response()->json(['token' => $token->plainTextToken]);
         }
 
-        return response(['status' => false], 401);
+        abort(401);
+    }
+
+    public function logout(): JsonResponse
+    {
+        $result = auth()->user()->currentAccessToken()->delete();
+
+        return response()->json(['status' => $result]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->controller(\App\Http\Controllers\TodoController::class)->prefix('todo-list')->group(function () {
     Route::post('add', 'store');
     Route::get('list', 'list');
     Route::get('show/{id}', 'show');
     Route::put('update/{id}', 'update');
-
     Route::patch('toggle-done/{id}', 'toggleDone');
-
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
